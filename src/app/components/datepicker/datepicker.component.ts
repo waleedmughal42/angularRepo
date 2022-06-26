0efbe8535936efb05e5de94323bbf7e325fd2c7e
Waleed Mughal <waleed48581@gmail.com>
2022-06-26 15:32:07 +0500

    added date picker

--- src/app/components/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datepicker.component.css']
 })
 export class DatepickerComponent implements OnInit {
-
+  sDatePickerValue: any;
   constructor() { }
 
   ngOnInit(): void {
+    console.log("loaded");
+    
+  }
+  cDatePickerComponent_SelectedDate(event:any)
+  {
+    console.log("Incomming Date picked ======>>>>>>>",event );
+    this.sDatePickerValue = event;
   }
 
 }
